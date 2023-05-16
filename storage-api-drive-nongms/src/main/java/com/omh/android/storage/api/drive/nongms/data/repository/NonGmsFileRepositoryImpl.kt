6007c39ee606a382625ad5203d62ce072fb0424c
Hector A. Narvaez <hector.narvaez@encora.com>
2023-05-16 18:26:12 -0500

    Make NonGmsFileRepositoryImpl internal

--- storage-api-drive-nongms/src/main/java/com/omh/android/storage/api/drive/nongms/data/repository/NonGmsFileRepositoryImpl.kt
+++ storage-api-drive-nongms/src/main/java/com/omh/android/storage/api/drive/nongms/data/repository/NonGmsFileRepositoryImpl.kt
@@ -3,7 +3,7 @@ package com.omh.android.storage.api.drive.nongms.data.repository
 import com.omh.android.storage.api.data.source.FileRemoteDataSource
 import com.omh.android.storage.api.domain.repository.FileRepository
 
-class NonGmsFileRepositoryImpl(
+internal class NonGmsFileRepositoryImpl(
     private val networkDataSource: FileRemoteDataSource
 ) : FileRepository {
 
