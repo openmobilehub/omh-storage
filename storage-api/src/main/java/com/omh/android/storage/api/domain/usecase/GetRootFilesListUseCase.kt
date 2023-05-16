22b7521cf1c1fe60cd8c109aa42d769d292922d7
Hector A. Narvaez <hector.narvaez@encora.com>
2023-05-16 18:13:12 -0500

    Refactor: rename GetAllFilesAndFoldersUseCase into GetRootFilesListUseCase

--- storage-api/src/main/java/com/omh/android/storage/api/domain/usecase/GetRootFilesListUseCase.kt
+++ storage-api/src/main/java/com/omh/android/storage/api/domain/usecase/GetRootFilesListUseCase.kt
@@ -2,7 +2,7 @@ package com.omh.android.storage.api.domain.usecase
 
 import com.omh.android.storage.api.domain.repository.FileRepository
 
-class GetAllFilesAndFoldersUseCase(
+class GetRootFilesListUseCase(
     private val repository: FileRepository
 ) : GetAllFilesAndFolders {
     override fun execute() = repository.getRootFilesList()
