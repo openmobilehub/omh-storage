25d3c91566b7e5544ec25daf4591c875f9c477a3
Hector A. Narvaez <hector.narvaez@encora.com>
2023-05-19 09:59:18 -0500

    Add class OmhResult.kt

---
+++ storage-api/src/main/java/com/omh/android/storage/api/domain/usecase/OmhResult.kt
@@ -0,0 +1,22 @@
+package com.omh.android.storage.api.domain.usecase
+
+sealed class OmhResult<out R> {
+
+    companion object {
+        internal const val TAG_SUCCESS = "[RESULT SUCCESS -> data: %s]"
+        internal const val TAG_ERROR = "[RESULT ERROR -> exception: %s]"
+    }
+
+    data class OmhSuccess<out T>(val data: T): OmhResult<T>()
+
+    data class OmhError(val exception: Exception): OmhResult<Nothing>()
+
+    override fun toString() = when(this) {
+        is OmhSuccess<*> -> String.format(TAG_SUCCESS, data.toString())
+        is OmhError -> String.format(TAG_ERROR, exception.toString())
+    }
+}
+
+object NoParams
+
+object NoResult
