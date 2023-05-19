80d389581ce87b37f68f6b4b2a96d94ef589a6d0
Hector A. Narvaez <hector.narvaez@encora.com>
2023-05-19 11:10:45 -0500

    Created generic abstract class for use cases

--- storage-api/src/main/java/com/omh/android/storage/api/domain/usecase/OmhSuspendUseCase.kt
+++ storage-api/src/main/java/com/omh/android/storage/api/domain/usecase/OmhSuspendUseCase.kt
@@ -1,7 +1,34 @@
 package com.omh.android.storage.api.domain.usecase
 
-import com.omh.android.storage.api.domain.model.OmhFile
+import android.util.Log
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 
-interface OmhSuspendUseCase {
-    fun execute(): List<OmhFile>
+abstract class OmhSuspendUseCase<in P, R>(private val coroutineDispatcher: CoroutineDispatcher) {
+
+    companion object {
+
+        private const val TAG_ERROR = "Error = "
+    }
+
+    @SuppressWarnings("TooGenericExceptionCaught")
+    suspend operator fun invoke(parameters: P): OmhResult<R> {
+        return try {
+            withContext(coroutineDispatcher) {
+                execute(parameters).let {
+                    OmhResult.OmhSuccess(it)
+                }
+            }
+        } catch (e: Exception) {
+            OmhResult.OmhError(e).also {
+                Log.e(TAG_ERROR, it.toString())
+            }
+        }
+    }
+
+    /**
+     * Override this to set the code to be executed.
+     */
+    @Throws(RuntimeException::class)
+    protected abstract suspend fun execute(parameters: P): R
 }
