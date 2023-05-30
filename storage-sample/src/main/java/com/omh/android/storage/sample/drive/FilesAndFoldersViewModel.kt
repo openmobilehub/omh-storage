91a9f7d3c521bd1925b5b03b8743edb778a60ca2
Hector A. Narvaez <hector.narvaez@encora.com>
2023-05-30 10:58:12 -0500

    Fix compilation error on sample app

--- storage-sample/src/main/java/com/omh/android/storage/sample/drive/FilesAndFoldersViewModel.kt
+++ storage-sample/src/main/java/com/omh/android/storage/sample/drive/FilesAndFoldersViewModel.kt
@@ -4,28 +4,31 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.omh.android.storage.api.domain.usecase.SortFilesAndFoldersByName
-import com.omh.android.storage.sample.model.UiFileFolderItemData
-import com.omh.android.storage.sample.mapper.fromFilesFoldersListToUi
-import com.omh.android.storage.sample.usecase.KtsGetAllFilesAndFolders
+import com.omh.android.storage.api.domain.model.OmhFile
+import com.omh.android.storage.api.domain.usecase.GetFilesListWithParentIdUseCase
+import com.omh.android.storage.api.domain.usecase.GetFilesListWithParentIdUseCaseParams
+import com.omh.android.storage.api.domain.usecase.OmhResult
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class FilesAndFoldersViewModel(
-    private val getAllFilesAndFolders: KtsGetAllFilesAndFolders,
-    private val sortFilesAndFoldersByName: SortFilesAndFoldersByName
+    private val getFilesListWithParentIdUseCase: GetFilesListWithParentIdUseCase
 ) : ViewModel() {
 
-    private val _filesFoldersLiveData = MutableLiveData<List<UiFileFolderItemData>>()
-    val filesFoldersLiveData: LiveData<List<UiFileFolderItemData>> = _filesFoldersLiveData
+    private val _filesFoldersLiveData = MutableLiveData<List<OmhFile>>()
+    val filesFoldersLiveData: LiveData<List<OmhFile>> = _filesFoldersLiveData
     private val _sortByNameAscendingLiveData = MutableLiveData<Boolean>()
     val sortByNameAscendingLiveData: LiveData<Boolean> = _sortByNameAscendingLiveData
 
     fun loadFilesFoldersFromRoot() {
         viewModelScope.launch(Dispatchers.IO) {
-            _filesFoldersLiveData.postValue(
-                fromFilesFoldersListToUi(getAllFilesAndFolders.execute())
-            )
+
+            when (val result = getFilesListWithParentIdUseCase(GetFilesListWithParentIdUseCaseParams())) {
+                is OmhResult.OmhSuccess -> {
+                    _filesFoldersLiveData.postValue(result.data.files)
+                }
+                is OmhResult.OmhError -> {}
+            }
         }
     }
 
