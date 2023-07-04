a163360cc23ebaf67bb22394f6604f4592ca0b2e
Hans Hamel <hans.hamel@encora.com>
2023-07-04 12:07:28 -0400

    Storage sdk: Remove parentId as paramater.

--- storage-api-drive-gms/src/main/java/com/omh/android/storage/api/drive/gms/data/source/GmsFileRemoteDataSourceImpl.kt
+++ storage-api-drive-gms/src/main/java/com/omh/android/storage/api/drive/gms/data/source/GmsFileRemoteDataSourceImpl.kt
@@ -68,7 +68,7 @@ internal class GmsFileRemoteDataSourceImpl(private val apiService: GoogleDriveAp
         return response.toOmhFile()
     }
 
-    override fun updateFile(localFileToUpload: File, fileId: String, parentId: String?) = null
+    override fun updateFile(localFileToUpload: File, fileId: String) = null
 
     private fun getStringMimeTypeFromLocalFile(file: File) = MimeTypeMap
         .getSingleton()
