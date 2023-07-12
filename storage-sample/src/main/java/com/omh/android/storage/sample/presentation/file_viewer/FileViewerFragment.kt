1fff31180aebca5182225f8557c5076f508311e4
Anton Soares <asoares@butterflynetinc.com>
2023-07-12 12:41:46 -0500

    small refactor.

--- storage-sample/src/main/java/com/omh/android/storage/sample/presentation/file_viewer/FileViewerFragment.kt
+++ storage-sample/src/main/java/com/omh/android/storage/sample/presentation/file_viewer/FileViewerFragment.kt
@@ -16,6 +16,7 @@ import android.widget.ArrayAdapter
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.MenuProvider
 import androidx.documentfile.provider.DocumentFile
@@ -114,13 +115,17 @@ class FileViewerFragment :
             }
         }
 
-        requireActivity().addMenuProvider(
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        val compatActivity: AppCompatActivity = activity as? AppCompatActivity ?: return
+        compatActivity.addMenuProvider(
             FileViewerMenuProvider(),
             viewLifecycleOwner,
             Lifecycle.State.RESUMED
         )
     }
-
 
     override fun onBackPressed(): Boolean {
         dispatchEvent(FileViewerViewEvent.BackPressed)
