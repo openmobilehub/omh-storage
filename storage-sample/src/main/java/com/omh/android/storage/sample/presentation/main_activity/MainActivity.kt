aaf5bc0c097a14101e99864014467fb54051d7a8
Anton Soares <asoares@butterflynetinc.com>
2023-07-12 11:37:16 -0500

    removed splash activity.

--- storage-sample/src/main/java/com/omh/android/storage/sample/presentation/main_activity/MainActivity.kt
+++ storage-sample/src/main/java/com/omh/android/storage/sample/presentation/main_activity/MainActivity.kt
@@ -1,44 +1,30 @@
 package com.omh.android.storage.sample.presentation.main_activity
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.omh.android.auth.api.OmhAuthClient
 import com.omh.android.storage.sample.R
 import com.omh.android.storage.sample.presentation.BaseActivity
 import com.omh.android.storage.sample.presentation.file_viewer.FileViewerFragment
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity(), FileViewerFragment.FileViewerFragmentListener {
 
-    companion object {
-
-        private const val IS_USER_LOGGED_EXTRA = "isUserLogged"
-        private const val IS_USER_LOGGED_EXTRA_DEFAULT_VALUE = false
-
-        fun getIntent(context: Context, isUserLogged: Boolean) =
-            Intent(context, MainActivity::class.java).apply {
-                putExtra(IS_USER_LOGGED_EXTRA, isUserLogged)
-            }
-    }
+    @Inject
+    lateinit var authClient: OmhAuthClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val isUserLogged = intent.getBooleanExtra(
-            IS_USER_LOGGED_EXTRA,
-            IS_USER_LOGGED_EXTRA_DEFAULT_VALUE
-        )
-
-        navigateTo(
-            if (isUserLogged) {
-                R.id.action_login_fragment_to_file_viewer_fragment
-            } else {
-                R.id.login_fragment
-            }
-        )
+        val screenId = if (authClient.getUser() != null) {
+            R.id.action_login_fragment_to_file_viewer_fragment
+        } else {
+            R.id.login_fragment
+        }
+        navigateTo(screenId)
     }
 
     override fun finishApplication() {
@@ -55,21 +41,10 @@ class MainActivity : BaseActivity(), FileViewerFragment.FileViewerFragmentListen
 
         if (fileViewerFragment != null) {
             when (item.itemId) {
-                R.id.swapGridOrLinear -> {
-                    fileViewerFragment.swapLayout()
-                }
-
-                R.id.createFile -> {
-                    fileViewerFragment.createFile()
-                }
-
-                R.id.uploadFile -> {
-                    fileViewerFragment.uploadFile()
-                }
-
-                R.id.signOut -> {
-                    fileViewerFragment.signOut()
-                }
+                R.id.swapGridOrLinear -> fileViewerFragment.swapLayout()
+                R.id.createFile -> fileViewerFragment.createFile()
+                R.id.uploadFile -> fileViewerFragment.uploadFile()
+                R.id.signOut -> fileViewerFragment.signOut()
             }
         }
 
