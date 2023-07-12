68a6d39b87c28670150ef7252865fa8b69a7fb8d
Anton Soares <asoares@butterflynetinc.com>
2023-07-12 16:14:50 -0500

    Added obfuscation rules.

--- storage-api-drive-nongms/src/main/java/com/omh/android/storage/api/drive/nongms/data/source/response/FileRemoteResponse.kt
+++ storage-api-drive-nongms/src/main/java/com/omh/android/storage/api/drive/nongms/data/source/response/FileRemoteResponse.kt
@@ -1,13 +1,15 @@
 package com.omh.android.storage.api.drive.nongms.data.source.response
 
+import androidx.annotation.Keep
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@Keep
 @JsonIgnoreProperties(ignoreUnknown = true)
 internal data class FileRemoteResponse(
-    @JsonProperty("mimeType")val mimeType: String?,
-    @JsonProperty("id")val id: String?,
-    @JsonProperty("name")val name: String?,
-    @JsonProperty("modifiedTime")val modifiedTime: String?,
-    @JsonProperty("parents")val parents: List<String>?
+    @JsonProperty("mimeType") val mimeType: String?,
+    @JsonProperty("id") val id: String?,
+    @JsonProperty("name") val name: String?,
+    @JsonProperty("modifiedTime") val modifiedTime: String?,
+    @JsonProperty("parents") val parents: List<String>?
 )
