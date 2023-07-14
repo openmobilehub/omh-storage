8144fde6a5de2c188ba776f92818aa261b9d8410
Anton Tchistiakova <anton.soares@encora.com>
2023-07-14 10:21:30 -0500

    Added causes to error handling where possible. (#76)

--- storage-api/src/main/java/com/omh/android/storage/api/domain/model/OmhStorageException.kt
+++ storage-api/src/main/java/com/omh/android/storage/api/domain/model/OmhStorageException.kt
@@ -2,22 +2,22 @@ package com.omh.android.storage.api.domain.model
 
 import com.omh.android.auth.api.models.OmhAuthStatusCodes
 
-sealed class OmhStorageException(val statusCode: Int) : Exception() {
+sealed class OmhStorageException(private val statusCode: Int) : Exception() {
 
     override val message: String?
         get() = OmhAuthStatusCodes.getStatusCodeString(statusCode)
 
     class InvalidCredentialsException(statusCode: Int) : OmhStorageException(statusCode)
 
-    class ApiException(statusCode: Int) : OmhStorageException(statusCode)
+    class ApiException(statusCode: Int, override val cause: Throwable? = null) : OmhStorageException(statusCode)
 
     class DownloadException(
         statusCode: Int,
-        override val message: String
+        override val cause: Throwable?
     ) : OmhStorageException(statusCode)
 
     class UpdateException(
         statusCode: Int,
-        override val message: String
+        override val cause: Throwable?
     ) : OmhStorageException(statusCode)
 }
